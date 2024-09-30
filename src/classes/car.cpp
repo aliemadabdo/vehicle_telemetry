291fa9f585e7d0bd20950fca24ca67f9bb536cff
Ali Emad <90727865+aliemadabdo@users.noreply.github.com>
2024-09-30 10:40:16 +0300

    document car class

--- src/classes/car.cpp
+++ src/classes/car.cpp
@@ -4,20 +4,38 @@
 #include "../headers/car.hpp"
 #include "../headers/logger.hpp"
 
+/**
+ * @brief Constructs a Car object and initializes its sensors.
+ * 
+ * @param sens A reference to a vector of unique pointers to Sensor objects.
+ */
 Car::Car(std::vector<std::unique_ptr<Sensor>>& sens) {
-    sensors = &sens;
+    this->sensors = &sens;
 }
 
-// Polymorphism 
+/**
+ * @brief Generates random values for all sensors.
+ * 
+ * This function iterates through all sensors and invokes the 
+ * setRandomValue method for each sensor. It uses references 
+ * to ensure unique ownership of sensor pointers.
+ */
 void Car::generateRandomVal(){
-    // due to unqieneess we can't assign the variable to more than one pointer 
-    // so we have to use refernces
+    // Due to uniqueness, we can't assign the variable to more than one pointer
+    // so we have to use references.
     for(const auto &s : *sensors){ 
-        s->setRandomValue();
+        s->setRandomValue(); // Polymorphism 
     }
 }
 
-// Polymorphism 
+/**
+ * @brief Displays the current values of all sensors.
+ * 
+ * This function iterates through all sensors and calls the 
+ * printValue method for each sensor to output its current state.
+ * Using `const` signals that the elements being iterated over 
+ * should not be modified.
+ */
 void Car::showData(){
     // Using `const` can make your code clearer in terms of intent.
     // It signals to other developers (or to yourself in the future) that
@@ -28,37 +46,54 @@ void Car::showData(){
     std::cout << std::endl;
 }
 
+/**
+ * @brief Performs diagnostics on the car's sensors.
+ * 
+ * This function checks the alert status of critical sensors, 
+ * such as engine temperature, battery temperature, and battery level. 
+ * If any sensor indicates a critical condition, an alert is logged 
+ * using the Logger instance.
+ */
 void Car::diagnostics(){
-    if ( (*sensors)[index::tempSensor].get()->isAlert()) 
-        Logger::getInstance()->alert("Engine temperature is critical! Initiating cooling...\n",0000);
-        // TODO: add relation between speed and engine temprature
-    if ( (*sensors)[index::batteryTemp].get()->isAlert()) 
-        Logger::getInstance()->alert("Battery temperature is critical! Initiating cooling...\n",0000);
-    if ( (*sensors)[index::batteryLvl].get()->isAlert()) 
-        Logger::getInstance()->alert("Low battery level! Please recharge...\n",0000);
-
+    if ((*sensors)[index::tempSensor]->isAlert()) 
+        Logger::getInstance()->alert("Engine temperature is critical! Initiating cooling...\n", 0000);
+        // TODO: add relation between speed and engine temperature
+    if ((*sensors)[index::batteryTemp]->isAlert()) 
+        Logger::getInstance()->alert("Battery temperature is critical! Initiating cooling...\n", 0000);
+    if ((*sensors)[index::batteryLvl]->isAlert()) 
+        Logger::getInstance()->alert("Low battery level! Please recharge...\n", 0000);
 }
 
+/**
+ * @brief Implements adaptive cruise control functionality.
+ * 
+ * This function checks the alert status of the speed sensor and radar sensor. 
+ * If either sensor indicates an alert, it applies brakes to maintain safety. 
+ * A dynamic cast is used to ensure that the correct sensor type is being handled.
+ */
 void Car::adaptiveCruiseControl() {
     // Correct dynamic cast
     SpeedSensor* speedSensor = dynamic_cast<SpeedSensor*>((*sensors)[index::speedSensor].get());
 
-    if ( (*sensors)[index::speedSensor].get()->isAlert()){
+    if ((*sensors)[index::speedSensor]->isAlert()){
         if (speedSensor)
             applyBrakes(speedSensor);
-        else{
-            Logger::getInstance()->debug("Invalid dynamic casting!\n",0000);
+        else {
+            Logger::getInstance()->debug("Invalid dynamic casting!\n", 0000);
             return;
         }
     }
 
-    if ( (*sensors)[index::radarSensor].get()->isAlert() ){
+    if ((*sensors)[index::radarSensor]->isAlert()) {
         if (speedSensor)
             applyBrakes(speedSensor);
     }  
     // TODO: add relation between speed and distance
 
-    // free(speedSensor); // this was deleting the allocted memory for the speedSensor object
+   // free(speedSensor); // Note: Using 'free' on a unique_ptr is not correct; consider removing this line.
 }
 
+/**
+ * @brief Destructs the Car object.
+ */
 Car::~Car(){}
